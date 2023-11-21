8c7f4b9a559e8b152a5ba236db57eeaf34fcc289
Johana <johanitabellota15a@gmail.com>
2023-11-20 23:34:16 -0500

    Version 3 se completo el CRUD en Base de Datos

--- src/protegido/AppForm.js
+++ src/protegido/AppForm.js
@@ -1,26 +1,85 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { addDoc, collection, doc, getDoc } from "firebase/firestore";
+import { db } from "../conexion/firebase";
 
 const AppForm = (props) => {
+
+  const handleStatusChange = (e) =>{
+    const {name, value} = e.target;
+    setObjeto({...objeto, [name]:value});
+    //console.log({name, value});
+    console.log(objeto);
+  }
+
+  const camposRegistro = {nombre:"",edad:"",genero:""};
+  const [objeto, setObjeto] = useState(camposRegistro);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      if (props.idActual =="" ){
+        if(validarform()){
+          addDoc(collection(db, 'basedatos'), objeto);
+          alert("Se registro con éxito...");
+        }else{
+          console.log("No se guardo...");
+        }
+      }else{
+        alert("Se actualizo el Registro...");
+      }
+    } catch (error) {
+      console.log("Error en Crear o Actualizar",error);    
+    }
+  }
+
+  const validarform =() => {
+    if(objeto.nombre === "" || /^\s+$/.test(objeto.nombre)){
+      alert("Escriba nombre...");
+      return false;
+    }
+    return true;
+  };
+
+  useEffect(() =>{
+    if(props.idActual === ""){
+      setObjeto({...camposRegistro});
+    }else{
+      obtenerDatosPorId(props.idActual);
+    }
+  },[props.idActual]);
+
+  const obtenerDatosPorId = async (xId) =>{
+    const objPorId = doc(db, "basedatos", xId);
+    const docPorId = await getDoc(objPorId);
+    if(docPorId.exists()){
+      setObjeto(docPorId.data());
+    }else{
+      console.log("No hay doc");
+    }
+  }
+
   return (
     <div style={{background:"orange", padding:"10px"}} >
-        <form>
+        <form onSubmit={handleSubmit} >
         <button>Cerrar aplicacion</button>
 
-        <h2>Registrar {AppForm.js}</h2>
-        <input name='nombre' type='text' placeholder='Nombres...' ></input><br/>
-        <input name='edad' type='text' placeholder='Edad...' ></input><br/>
+        <h2>Registrar (AppForm.js)</h2>
+        
+        <input onChange={handleStatusChange} value={objeto.nombre} name='nombre' type='text' placeholder='Nombres...' ></input><br/>
+        <input onChange={handleStatusChange} value={objeto.edad} name='edad' type='text' placeholder='Edad...' ></input><br/>
 
-        <select>
+        <select onChange={handleStatusChange} value={objeto.genero} name='genero' >
             <option value="">Selecione género...</option>
             <option value="Masculino">Masculino</option>
             <option value="Femenino">Femenino</option>
         </select><br/>
 
 
-        <button>Guardar / Actualizar</button>
+        <button>
+          {props.idActual=="" ? "Guardar": "Actualizar"}
+        </button>
         </form>
     </div>
   )
 }
 
-export default AppForm
+export default AppForm;
--- src/protegido/AppLista.js
+++ src/protegido/AppLista.js
@@ -1,19 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import App from '../App'
 import AppForm from './AppForm'
+import { collection, deleteDoc, doc, onSnapshot, query } from 'firebase/firestore';
+import { db } from '../conexion/firebase';
 
 const AppLista = (props) => {
+  const [docBD, setDocBD] = useState([]);
+  const fnRead = () =>{
+    const xColeccionConQuey = query(collection(db, 'basedatos'));
+    const unsubcribe = onSnapshot(xColeccionConQuey, (xDatosBD)=>{
+      const xDoc = [];
+      xDatosBD.forEach((doc) => {
+        xDoc.push({id:doc.id, ...doc.data()});
+      });
+      setDocBD(xDoc);
+    });
+  }
+  useEffect(() =>{fnRead();}, [props.idActual]);
+  //console.log(docBD);
+
+  const [idActual, setIdActual] = useState("");
+  const fnDelete = async (xId) =>{
+    if (window.confirm("Confirme para eliminar")){
+      await deleteDoc(doc(db, "basedatos", xId));
+    }
+    alert("Se elimino con éxito...");
+  }
+
   return (
     <div style={{background:"greenyellow", padding:"10px"}}>
       <h1>App.js</h1>
-      <AppForm/>
+      <AppForm {...{idActual, setIdActual}} />
 
       <h1>Lista de Clientes</h1>
-      <p>No.1. Juan Manuel Ticona Vega .....x......A </p>
-      <p>No.2. Rosa Maria Luque Conde .....x......A </p>
-      <p>No.3. Joe Ricardo Lopez Moral .....x......A </p>
+      {
+        docBD.map((row, index) => 
+        <p key={row.id}>
+          No.{index + 1}. {row.nombre} 
+          .....
+          <span onClick={()=> fnDelete(row.id)} >x</span> 
+          .....
+          <span onClick={()=> setIdActual(row.id)} >A</span>
+          A 
+          </p> 
+          )
+      }
+      
     </div>
   )
 }
 
-export default AppLista
+export default AppLista;
